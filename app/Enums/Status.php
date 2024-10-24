3ed2341a3b514473ededaeb60a20e7e836d1ceab
azhagiri <azhagirishankar5@gmail.com>
2024-10-24 19:04:54 +0530

    #31 work on tasks show page

--- app/Enums/Status.php
+++ app/Enums/Status.php
@@ -21,11 +21,11 @@ enum Status : int
             self::NEW => 'New',
             self::PENDING => 'Pending',
             self::ONPROGRESS => 'On Progress',
-            self::NOTSTARTED => 'Notstarted',
+            self::NOTSTARTED => 'Not started',
             self::ARCHIVED => 'Archived',
             self::FINISHED => 'Finished',
             self::CLOSED => 'Closed',
-            self::NOTACTIVE => 'NotActive',
+            self::NOTACTIVE => 'Not Active',
             self::RECEIVED => 'Received',
             self::ADMINCLOSED => 'Sundar Closed',
         };
@@ -34,16 +34,16 @@ enum Status : int
     public function getStyle(): string
     {
         return match ($this) {
-            self::NEW => 'text-white bg-green-500',
-            self::PENDING => 'text-white bg-red-500',
-            self::ONPROGRESS => 'text-white bg-orange-500',
-            self::NOTSTARTED => 'text-white bg-amber-500',
-            self::ARCHIVED => 'text-white bg-emerald-500',
-            self::FINISHED => 'text-white bg-gray-500',
-            self::CLOSED => 'text-white bg-sky-500',
-            self::NOTACTIVE => 'text-white bg-rose-500',
-            self::RECEIVED => 'text-white bg-indigo-600',
-            self::ADMINCLOSED => 'text-white bg-violet-100',
+            self::NEW => 'text-cyan-700 bg-cyan-100 border border-cyan-400',
+            self::PENDING => 'text-yellow-700 bg-yellow-100  border border-yellow-400',
+            self::ONPROGRESS => 'text-green-700 bg-green-50 border border-green-400',
+            self::NOTSTARTED => 'text-slate-700 bg-slate-100 border border-slate-400',
+            self::ARCHIVED => 'text-gray-700 bg-gray-200 border border-gray-400',
+            self::FINISHED => 'text-red-700 bg-red-100 border border-red-200',
+            self::CLOSED => 'text-slate-700 bg-slate-200 border border-slate-400',
+            self::NOTACTIVE => 'text-purple-700 bg-purple-100 border border-purple-400',
+            self::RECEIVED => 'text-sky-700 bg-sky-100 border border-sky-400',
+            self::ADMINCLOSED => 'text-rose-700 bg-rose-100 border border-rose-400',
         };
     }
 
