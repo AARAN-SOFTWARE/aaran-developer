65aea030db960b6b5c2c1c2694f65591ee1d7eb4
azhagiri <azhagirishankar5@gmail.com>
2024-11-23 13:37:41 +0530

    #48 created Client Folder in Aaran

--- aaran/Client/Providers/ClientServiceProvider.php
+++ aaran/Client/Providers/ClientServiceProvider.php
@@ -4,12 +4,12 @@ namespace Aaran\Common\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
-class CommonServiceProvider extends ServiceProvider
+class ClientServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
-        $this->mergeConfigFrom(__DIR__ . '/../config.php','common');
+        $this->mergeConfigFrom(__DIR__ . '/../config.php','client');
 
         $this->app->register(ClientRouteServiceProvider::class);
     }
