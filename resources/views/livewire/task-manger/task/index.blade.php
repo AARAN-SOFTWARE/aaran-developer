074d9802522108620512ea3dd5967e562eccda30
Divya <divyadharshini146@gmail.com>
2024-12-04 20:07:36 +0530

    #72 Refactor Task....

--- resources/views/livewire/task-manger/task/index.blade.php
+++ resources/views/livewire/task-manger/task/index.blade.php
@@ -24,7 +24,7 @@
             </div>
 
             <div class="w-80 ">
-                <x-input.model-select wire:model.live="jobFilter" :label="'Year'">
+                <x-input.model-select wire:model.live="jobFilter" :label="'Job'">
                     <option value="">Select Job</option>
                     @foreach($jobCollection as $job)
                         <option value="{{ $job->id }}">{{ $job->vname }}</option>
